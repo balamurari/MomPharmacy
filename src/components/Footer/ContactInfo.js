a7a5628dc80462d44b9bacc018099f0b94b59197
balamurari <balamurari2003@gmail.com>
2024-12-11 23:19:00 +0530

    updated footer and function to it

--- src/components/Footer/ContactInfo.js
+++ src/components/Footer/ContactInfo.js
@@ -6,15 +6,27 @@ function ContactInfo() {
     <div className="space-y-2">
       <div className="flex items-center gap-2 text-gray-400">
         <Phone size={20} />
-        <span>+91 77020 68334</span>
+        <a href="tel:+917702068334" className="hover:text-blue-500">
+          +91 77020 68334
+        </a>
       </div>
       <div className="flex items-center gap-2 text-gray-400">
         <Mail size={20} />
-        <span>Ps@mompharmacy.in</span>
+        <a href="mailto:Ps@mompharmacy.in" className="hover:text-blue-500">
+          Ps@mompharmacy.in
+        </a>
       </div>
       <div className="flex items-center gap-2 text-gray-400">
-        <MapPin size={40} />
-        <span>No.3, 1st Main Road Hebbal, Kempapura, Bangalore - 560024 </span>
+        <MapPin size={20} />
+        <a 
+          href="https://www.google.com/maps/search/?api=1&query=1st+Main,+Netajinagar,+Kempapura,+Bangalore,+560024" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="hover:text-blue-500"
+        >
+          1st Main, Netajinagar, Kempapura, Bangalore - 560024
+        </a>
+
       </div>
     </div>
   );
